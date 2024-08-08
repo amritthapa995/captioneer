f55d099a42735a6b69104a633a63da214818eaf1
Amrit Thapa <amritthapa@Amrits-MacBook-Air.local>
2024-08-08 21:36:47 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/captioneer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.captioneer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:captioneer/Screens/signup_screen.dart';
+import 'package:captioneer/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "CapGen",
+              style: TextStyle(
+                  height: 3, fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 18),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              child: const Text('Login'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignupScreen()),
+                );
+              },
+              child: const Text('Continue with Google'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account? "),
+                InkWell(
+                  child: Text("Signup"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignupScreen()),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text("I have read all terms and conditions"),
+          ],
+        ),
+      ),
+    );
+  }
+}
